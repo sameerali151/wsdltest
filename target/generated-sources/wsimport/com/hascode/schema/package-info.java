a4deaa843a543f1aa2090a424efcb0a3292b1e88
sameerali151 <47702284+sameerali151@users.noreply.github.com>
2020-02-13 18:29:17 -0600

    add changes

---
+++ target/generated-sources/wsimport/com/hascode/schema/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://codenotfound.com/types/helloworld", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.hascode.schema;
